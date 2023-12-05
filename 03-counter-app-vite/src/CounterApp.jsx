9495d53a3267c2d5fc08ca14d612562948661046
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-12-05 12:33:32 +0100

    Video-47

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -3,15 +3,26 @@ import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
 	const [counter, setCounter] = useState(value);
+
 	const handleAdd = () => {
-		// console.log(event);
 		setCounter(counter + 1);
 	};
+
+	const handleSubtract = () => {
+		setCounter(counter - 1);
+	};
+
+	const handleReset = () => {
+		setCounter(value);
+	};
+
 	return (
 		<>
 			<h1>CounterApp</h1>
 			<h2> {counter} </h2>
 			<button onClick={handleAdd}>+1</button>
+			<button onClick={handleSubtract}>-1</button>
+			<button onClick={handleReset}>Reset</button>
 		</>
 	);
 };
