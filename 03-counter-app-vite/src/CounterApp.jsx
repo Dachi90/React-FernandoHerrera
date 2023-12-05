8a8c1940014a40631037e78ff25209d242f98284
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-12-05 12:22:19 +0100

    Video-46

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const handleAdd = (event) => {
-	console.log(event);
-};
-
 export const CounterApp = ({ value }) => {
+	const [counter, setCounter] = useState(value);
+	const handleAdd = () => {
+		// console.log(event);
+		setCounter(counter + 1);
+	};
 	return (
 		<>
 			<h1>CounterApp</h1>
-			<h2> {value} </h2>
-			<button onClick={(event) => handleAdd(event)}>+1</button>
+			<h2> {counter} </h2>
+			<button onClick={handleAdd}>+1</button>
 		</>
 	);
 };
