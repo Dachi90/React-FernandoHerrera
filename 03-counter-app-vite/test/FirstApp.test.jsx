f3f79a89f27ca4ac2b5470b3692ecfcceea19d1a
Martín PC <bergesio90@gmail.com>
2023-12-08 09:05:31 +0100

    Video-65

--- 03-counter-app-vite/test/FirstApp.test.jsx
+++ 03-counter-app-vite/test/FirstApp.test.jsx
@@ -4,6 +4,18 @@ import { FirstApp } from '../src/FirstApp';
 describe('Pruebas en <FirstApp />', () => {
 	test('Debe de hacer match con el snapshot', () => {
 		const title = 'Hola, Soy Goku';
-		render(<FirstApp title={title} />);
+		const { container } = render(<FirstApp title={title} />);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	test('Debe de mostrar el título en un h1', () => {
+		const title = 'Hola, Soy Goku';
+		const { container, getByText } = render(<FirstApp title={title} />);
+
+		expect(getByText(title)).toBeTruthy();
+		const h1 = container.querySelector('h1');
+		console.log(h1.innerHTML);
+		expect(h1.innerHTML).toContain(title);
 	});
 });
