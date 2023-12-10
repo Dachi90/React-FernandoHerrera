c6aa818426dcc900854e461e49b367492853b5e1
Martín PC <bergesio90@gmail.com>
2023-12-10 17:52:51 +0100

    Video-77

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
-	console.log(categories);
+	const onAddCategory = () => {
+		//setCategories([...categories, 'Super Campeones']);
+		setCategories(['Super Campeones', ...categories]);
+	};
 
 	return (
 		<>
@@ -12,6 +15,7 @@ export const GifExpertApp = () => {
 			{/* Input */}
 
 			{/* Listado de Gifs */}
+			<button onClick={onAddCategory}>Agregar</button>
 			<ol>
 				{categories.map((category) => {
 					return <li key={category}>{category}</li>;
