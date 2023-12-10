e6fd6a47042462dbcefb36e61e7bc47f08c593e4
Martín PC <bergesio90@gmail.com>
2023-12-10 18:11:31 +0100

    Video-78

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
@@ -13,6 +14,7 @@ export const GifExpertApp = () => {
 			<h1>GifExpertApp</h1>
 
 			{/* Input */}
+			<AddCategory />
 
 			{/* Listado de Gifs */}
 			<button onClick={onAddCategory}>Agregar</button>
