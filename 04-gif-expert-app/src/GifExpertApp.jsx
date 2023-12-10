e54fea9eec72f6140e2b47e4b85cfa412b1dbf8d
Martín PC <bergesio90@gmail.com>
2023-12-10 20:59:38 +0100

    Video-82

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'Dragon ball']);
 	const onAddCategory = (newCategory) => {
 		console.log(newCategory);
-		//setCategories([...categories, newCategory]);
 		if (categories.includes(newCategory)) return;
 		setCategories([newCategory, ...categories]);
 	};
 
 	return (
 		<>
-			{/* título */}
 			<h1>GifExpertApp</h1>
 
-			{/* Input */}
-			<AddCategory
-				// setCategories={setCategories}
-				onNewCategory={(value) => onAddCategory(value)}
-			/>
+			<AddCategory onNewCategory={(value) => onAddCategory(value)} />
 
-			{/* Listado de Gifs */}
-			<ol>
-				{categories.map((category) => {
-					return <li key={category}>{category}</li>;
-				})}
-			</ol>
-			{/* Gif Item */}
+			{categories.map((category) => (
+				<GifGrid key={category} category={category} />
+			))}
 		</>
 	);
 };
