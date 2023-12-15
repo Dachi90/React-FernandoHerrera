c3840ea00edbdc3fd3f7b830855d93f85ef8846b
Martín PC <bergesio90@gmail.com>
2023-12-15 20:53:24 +0100

    Video-108

--- 04-gif-expert-app/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app/test/components/AddCategory.test.jsx
@@ -12,9 +12,10 @@ describe('Pruebas en <AddCategory />', () => {
 
 	test('Debe de llamar onNewCategory si el input tiene un valor', () => {
 		const inputValue = 'Saitama';
-		//TODO: ???
 
-		render(<AddCategory onNewCategory={() => {}} />);
+		const onNewCategory = jest.fn();
+
+		render(<AddCategory onNewCategory={onNewCategory} />);
 
 		const input = screen.getByRole('textbox');
 		const form = screen.getByRole('form');
@@ -23,5 +24,20 @@ describe('Pruebas en <AddCategory />', () => {
 		fireEvent.submit(form);
 
 		expect(input.value).toBe('');
+
+		expect(onNewCategory).toHaveBeenCalled();
+		//expect(onNewCategory).toHaveBeenCalledTimes(3);
+		expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+	});
+
+	test('No debe de llamar el onNewCategory si el input está vacío', () => {
+		const onNewCategory = jest.fn();
+		render(<AddCategory onNewCategory={onNewCategory} />);
+
+		const form = screen.getByRole('form');
+		fireEvent.submit(form);
+
+		expect(onNewCategory).toHaveBeenCalledTimes(0);
+		expect(onNewCategory).not.toHaveBeenCalled();
 	});
 });
