2b8a0273e202d11423698bb7b3e490f32e4fe1f6
Martín PC <bergesio90@gmail.com>
2023-12-16 11:39:44 +0100

    Video-109

---
+++ 04-gif-expert-app/test/components/GifGrid.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+
+describe('Pruebas en el archivo <GifGrid/>', () => {
+	const category = 'One Punch';
+	test('Debe de mostrar el loading inicialmente', () => {
+		render(<GifGrid category={category} />);
+		expect(screen.getByText('Cargando...'));
+		expect(screen.getByText(category));
+	});
+
+	test('Debe mostrar items cuandos se cargan imagenes desde el useFetchGifs', () => {});
+});
