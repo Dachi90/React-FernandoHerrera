cc2aa5040ad79393b6a68bc82c430a1e95c6ad78
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-12-18 16:19:32 +0100

    Video-124

---
+++ 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useForm } from '../hooks/useForm';
+
+export const FormWithCustomHook = () => {
+	const { formState, onInputChange, username, email, password } = useForm({
+		username: '',
+		email: '',
+		password: '',
+	});
+
+	return (
+		<>
+			<h1>Formlario con custom hook</h1>
+			<hr />
+			<input type='text' className='form-control' placeholder='Username' name='username' value={username} onChange={onInputChange} />
+			<input type='email' className='form-control mt-2' placeholder='fernando@google.ecom' name='email' value={email} onChange={onInputChange} />
+			<input type='password' className='form-control mt-2' placeholder='contraseña' name='password' value={password} onChange={onInputChange} />
+		</>
+	);
+};
