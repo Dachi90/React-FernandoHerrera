024b5eff4c2e976cfc7f102a225589784f45492d
Martín PC <bergesio90@gmail.com>
2023-12-18 19:39:21 +0100

    Video-129

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,11 @@
-import { useCounter } from '../hooks/useCounter';
-import { useFetch } from '../hooks/useFetch';
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote } from './LoadingQuote';
+import { Quote } from './Quote';
 
 export const MultipleCustomHooks = () => {
 	const { increment, counter } = useCounter(1);
 	const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+	const { author, quote } = !!data && data[0];
 
 	//console.log({ data, isLoading, hasError });
 
@@ -12,14 +14,7 @@ export const MultipleCustomHooks = () => {
 			<h1>BreakingBad Quotes</h1>
 			<hr />
 
-			{isLoading ? (
-				<div className='alert alert-info text-center'>Loading...</div>
-			) : (
-				<blockquote className='blockquote text-end'>
-					<p className='mb-1'>{data[0].quote}</p>
-					<footer className='blockquote-footer'> {data[0].author}</footer>
-				</blockquote>
-			)}
+			{isLoading ? <LoadingQuote /> : <Quote author={author} quote={quote} />}
 
 			<button className='btn btn-primary' onClick={() => increment()} disabled={isLoading}>
 				Next quote
