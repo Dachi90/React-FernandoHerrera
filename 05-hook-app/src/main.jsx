61a8075e138a7a51b3dd8dd50e286f2c0e229207
Martín PC <bergesio90@gmail.com>
2023-12-22 11:01:58 +0100

    Video-141

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -14,9 +14,10 @@ import './index.css';
 //import { MemoHook } from './06-memos/MemoHook';
 //import { CallBackHook } from './06-memos/CallBackHook';
 import { Padre } from './07-tarea-memo/Padre';
+import './08-useReducer/intro-reducer';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	// <React.StrictMode>
-	<Padre />
-	// </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// 	// <React.StrictMode>
+// 	<Padre />
+// 	// </React.StrictMode>
+// );
