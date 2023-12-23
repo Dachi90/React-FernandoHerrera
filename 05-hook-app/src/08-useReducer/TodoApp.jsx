c8411a616cd97041aa6a4188d31acca208bb6b90
Martín PC <bergesio90@gmail.com>
2023-12-23 12:35:56 +0100

    Video-148

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -2,22 +2,26 @@ import { useReducer } from 'react';
 import { todoReducer } from './TodoReducer';
 import { TodoList } from './components/TodoList';
 import { TodoAdd } from './components/TodoAdd';
+import { useEffect } from 'react';
 
 const initialState = [
-	{
-		id: new Date().getTime(),
-		description: 'Recolectar la piedra del alma',
-		done: false,
-	},
-	{
-		id: new Date().getTime() * 3,
-		description: 'Recolectar la piedra del tiempo',
-		done: false,
-	},
+	// {
+	// 	id: new Date().getTime(),
+	// 	description: 'Recolectar la piedra del alma',
+	// 	done: false,
+	// }
 ];
 
+const init = () => {
+	return JSON.parse(localStorage.getItem('todos') || []);
+};
+
 export const TodoApp = () => {
-	const [todos, dispatch] = useReducer(todoReducer, initialState);
+	const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
 	const handleNewTodo = (todo) => {
 		const action = {
