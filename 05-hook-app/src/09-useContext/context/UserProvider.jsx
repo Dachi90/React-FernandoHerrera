d1ef0ed052b954c36ee2fb2f203b436a5c114b70
Martín PC <bergesio90@gmail.com>
2023-12-27 12:16:22 +0100

    Video-161

---
+++ 05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -0,0 +1,10 @@
+import { UserContext } from './UserContext';
+
+const user = {
+	id: 123,
+	name: 'Fernando Herrera',
+	email: 'fernando@email.com',
+};
+export const UserProvider = ({ children }) => {
+	return <UserContext.Provider value={{ hola: 'Mundo', user: user }}>{children}</UserContext.Provider>;
+};
