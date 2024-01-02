8abc35ffb4b9e8d56a2dac5a0504fc42dce20a6e
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-02 14:51:02 +0100

    Video-196

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,12 +1,13 @@
+import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers';
 
 export const HeroList = ({ publisher }) => {
 	const heroes = getHeroesByPublisher(publisher);
 	return (
-		<ul>
-			{heroes.map((hero) => {
-				return <li key={hero.id}>{hero.superhero}</li>;
-			})}
-		</ul>
+		<div className='row row-cols-1 row-cols-md-3 g-3'>
+			{heroes.map((hero) => (
+				<HeroCard key={hero.id} {...hero} />
+			))}
+		</div>
 	);
 };
