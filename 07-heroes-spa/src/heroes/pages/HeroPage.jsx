6f5e14739495823f9c9abb774334ee49738607e5
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-02 12:14:37 +0100

    Video-193

---
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,7 @@
+export const HeroIdPage = () => {
+	return (
+		<>
+			<h1>Hero Page</h1>
+		</>
+	);
+};
---
+++ 07-heroes-spa/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../../ui';
+import { DcPage, HeroIdPage, MarvelPage, SearchPage } from '../pages';
+
+export const HeroesRoutes = () => {
+	return (
+		<>
+			<Navbar />
+
+			<div className='container'>
+				<Routes>
+					<Route path='marvel' element={<MarvelPage />} />
+					<Route path='dc' element={<DcPage />} />
+					<Route path='search' element={<SearchPage />} />
+					<Route path='hero' element={<HeroIdPage />} />
+
+					{/*Search, Hero by Id */}
+
+					<Route path='/' element={<Navigate to={'/marvel'} />} />
+				</Routes>
+			</div>
+		</>
+	);
+};
