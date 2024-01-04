fb262179817f3266a73c693c69370680b758febb
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-04 10:27:49 +0100

    Video-216

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,13 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
+import { PrivateRoutes } from './PrivateRoutes';
+import { PublicRoutes } from './PublicRoutes';
 
 export const AppRouter = () => {
 	return (
 		<>
 			<Routes>
+				<Route
+					path='/login'
+					element={
+						<PublicRoutes>
+							<LoginPage />
+						</PublicRoutes>
+					}
+				/>
 				<Route path='login' element={<LoginPage />} />
-				<Route path='/*' element={<HeroesRoutes />} />
+
+				<Route
+					path='/*'
+					element={
+						<PrivateRoutes>
+							<HeroesRoutes />
+						</PrivateRoutes>
+					}
+				/>
+				{/* <Route path='/*' element={<HeroesRoutes />} /> */}
 			</Routes>
 		</>
 	);
