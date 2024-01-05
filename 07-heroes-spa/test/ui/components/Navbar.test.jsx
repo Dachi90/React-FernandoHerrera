55c7efb424c8c81cbed0767f1313279e27dfd7f1
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-05 14:30:36 +0100

    Video-230

---
+++ 07-heroes-spa/test/ui/components/Navbar.test.jsx
@@ -0,0 +1,50 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Navbar } from '../../../src/ui/components/Navbar';
+import { MemoryRouter, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../src/auth/context/AuthContext';
+
+describe('Pruebas en <Navbar/>', () => {
+	const contextValue = {
+		logged: true,
+		user: {
+			id: 'ABC',
+			name: 'Juan Carlos',
+		},
+		logout: jest.fn(),
+	};
+	const mockedUseNavigate = jest.fn();
+
+	jest.mock('react-router-dom', () => ({
+		...jest.requireActual('react-router-dom'),
+		useNavigate: () => mockedUseNavigate,
+	}));
+
+	beforeEach(() => jest.clearAllMocks());
+
+	test('Debe de mostrar el nombre del usuario', () => {
+		render(
+			<MemoryRouter initialEntries={['/marvel']}>
+				<AuthContext.Provider value={contextValue}>
+					<Navbar />
+				</AuthContext.Provider>
+			</MemoryRouter>
+		);
+
+		expect(screen.getByText(contextValue.user.name)).toBeTruthy();
+	});
+
+	test('Debe de llamar al logout y navigate cuando se hace click en el botón', () => {
+		render(
+			<MemoryRouter initialEntries={['/marvel']}>
+				<AuthContext.Provider value={contextValue}>
+					<Navbar />
+				</AuthContext.Provider>
+			</MemoryRouter>
+		);
+
+		const button = screen.getByRole('button');
+		fireEvent.click(button);
+		expect(contextValue.logout).toHaveBeenCalled();
+		expect(mockedUseNavigate).toHaveBeenCalledWith('/login', { replace: true });
+	});
+});
