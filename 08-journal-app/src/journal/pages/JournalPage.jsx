1636c1088e336a00a1bc252386306a518fb25cb5
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-12 14:26:49 +0100

    Video-250

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
 	return (
 		<JournalLayout>
-			<Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse iusto ipsum nulla ratione omnis. Laudantium minima tenetur officiis ad! Voluptate optio temporibus exercitationem. Odio ad exercitationem accusantium dolorem eaque similique.</Typography>
+			{/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse iusto ipsum nulla ratione omnis. Laudantium minima tenetur officiis ad! Voluptate optio temporibus exercitationem. Odio ad exercitationem accusantium dolorem eaque similique.</Typography> */}
 
 			{/* Nothing selected */}
+			<NothingSelectedView />
+
 			{/* NoteView */}
 		</JournalLayout>
 	);
