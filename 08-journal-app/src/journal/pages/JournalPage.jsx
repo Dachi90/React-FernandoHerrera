68f9bcd9edec1b133f84e0d7def74467b6d46f55
Martín PC <bergesio90@gmail.com>
2024-01-13 10:45:37 +0100

    Video-253

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,16 +1,30 @@
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
 	return (
 		<JournalLayout>
 			{/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse iusto ipsum nulla ratione omnis. Laudantium minima tenetur officiis ad! Voluptate optio temporibus exercitationem. Odio ad exercitationem accusantium dolorem eaque similique.</Typography> */}
 
-			{/* Nothing selected */}
-			{/* <NothingSelectedView /> */}
+			<NothingSelectedView />
 
-			{/* NoteView */}
-			<NoteView />
+			{/* <NoteView /> */}
+
+			<IconButton
+				size='large'
+				sx={{
+					color: 'white',
+					backgroundColor: 'error.main',
+					':hover': { backgroundColor: 'error.main', opacity: 0.7 },
+					position: 'fixed',
+					right: 50,
+					bottom: 50,
+				}}
+			>
+				<AddOutlined sx={{ fontSize: 30 }} />
+			</IconButton>
 		</JournalLayout>
 	);
 };
