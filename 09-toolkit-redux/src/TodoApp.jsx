ed4c44e8ada96729f8a405ac4e80622949e8b4c9
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-16 12:43:34 +0100

    Video-270

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,7 +1,20 @@
-import { useGetTodosQuery } from './store/apis';
+import { useState } from 'react';
+import { useGetTodoQuery, useGetTodosQuery } from './store/apis';
 
 export const TodoApp = () => {
-	const { data: todos = [], isLoading } = useGetTodosQuery();
+	const [todoId, setTodoId] = useState(1);
+
+	//const { data: todo = [], isLoading } = useGetTodosQuery(1);
+	const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+	const nextTodo = () => {
+		setTodoId(todoId + 1);
+	};
+
+	const prevTodo = () => {
+		if (todoId === 1) return;
+		setTodoId(todoId - 1);
+	};
 
 	return (
 		<>
@@ -9,18 +22,19 @@ export const TodoApp = () => {
 			<hr />
 			<h4>IsLoading {isLoading ? 'true' : 'false'}</h4>
 
-			<pre>...</pre>
+			<pre>{JSON.stringify(todo)}</pre>
 
-			<ul>
+			<button onClick={prevTodo}>Prev TODO</button>
+			<button onClick={nextTodo}>Next TODO</button>
+
+			{/* <ul>
 				{todos.map((todo) => (
 					<li key={todo.id}>
 						<strong>{todo.completed ? 'DONE ' : 'PENDING '}</strong>
 						{todo.title}
 					</li>
 				))}
-			</ul>
-
-			<button>Next TODO</button>
+			</ul> */}
 		</>
 	);
 };
