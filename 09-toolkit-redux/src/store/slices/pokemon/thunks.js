f168b0e9d906b48173790c370866036532884271
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-16 11:25:47 +0100

    Video-267

--- 09-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,7 +1,7 @@
 import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokmonSlice';
 
-export const getPkemons = (page = 0) => {
+export const getPokemons = (page = 0) => {
 	return async (dispatch, getState) => {
 		dispatch(startLoadingPokemons());
 
@@ -9,7 +9,7 @@ export const getPkemons = (page = 0) => {
 		// const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=%${page * 10}`);
 		// const data = await resp.json();
 
-		const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=%${page * 10}`);
+		const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
 		console.log(data);
 
 		dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
