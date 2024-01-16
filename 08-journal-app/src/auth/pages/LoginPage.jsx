1641fe702503d48a3b639b27263f97ef3f06cc40
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-16 16:47:05 +0100

    Video-277

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,37 +1,111 @@
+import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { chekingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
+	const dispatch = useDispatch();
+
+	const { email, password, onInputChange } = useForm({
+		email: 'fernando@google.com',
+		password: '123456',
+	});
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		console.log({ email, password });
+
+		dispatch(chekingAuthentication());
+	};
+
+	const onGoogleSignIn = () => {
+		console.log('onGoogleSignIn');
+		dispatch(startGoogleSignIn());
+	};
+
 	return (
 		<AuthLayout title='Login'>
-			<form>
+			<form onSubmit={onSubmit}>
 				<Grid container>
-					<Grid item xs={12} sx={{ mt: 2 }}>
-						<TextField label='Correo' type='email' placeholder='correo@google.com' fullWidth />
+					<Grid
+						item
+						xs={12}
+						sx={{ mt: 2 }}
+					>
+						<TextField
+							label='Correo'
+							type='email'
+							placeholder='correo@google.com'
+							fullWidth
+							name='email'
+							value={email}
+							onChange={onInputChange}
+						/>
 					</Grid>
-					<Grid item xs={12} sx={{ mt: 2 }}>
-						<TextField label='Contraseña' type='password' placeholder='contraseña' fullWidth />
+					<Grid
+						item
+						xs={12}
+						sx={{ mt: 2 }}
+					>
+						<TextField
+							label='Contraseña'
+							type='password'
+							placeholder='contraseña'
+							fullWidth
+							name='password'
+							value={password}
+							onChange={onInputChange}
+						/>
 					</Grid>
 
-					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-						<Grid item xs={12} sm={6}>
-							<Button variant='contained' fullWidth>
+					<Grid
+						container
+						spacing={2}
+						sx={{ mb: 2, mt: 1 }}
+					>
+						<Grid
+							item
+							xs={12}
+							sm={6}
+						>
+							<Button
+								variant='contained'
+								fullWidth
+								type='submit'
+							>
 								Login
 							</Button>
 						</Grid>
 
-						<Grid item xs={12} sm={6}>
-							<Button variant='contained' fullWidth>
+						<Grid
+							item
+							xs={12}
+							sm={6}
+						>
+							<Button
+								variant='contained'
+								fullWidth
+								onClick={onGoogleSignIn}
+							>
 								<Google />
 								<Typography sx={{ ml: 1 }}>Google</Typography>
 							</Button>
 						</Grid>
 					</Grid>
 
-					<Grid container direction='row' justifyContent='end'>
-						<Link component={RouterLink} color='inherit' to='/auth/register'>
+					<Grid
+						container
+						direction='row'
+						justifyContent='end'
+					>
+						<Link
+							component={RouterLink}
+							color='inherit'
+							to='/auth/register'
+						>
 							Crear una cuenta
 						</Link>
 					</Grid>
---
+++ 08-journal-app/src/store/auth/thunks.js
@@ -0,0 +1,13 @@
+import { checkingCredentials } from './authSlice';
+
+export const chekingAuthentication = (email, password) => {
+	return async (dispatch) => {
+		dispatch(checkingCredentials());
+	};
+};
+
+export const startGoogleSignIn = () => {
+	return async (dispatch) => {
+		dispatch(checkingCredentials());
+	};
+};
