a8468b8548da4bb16aa247623573577a2778d834
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-16 12:28:44 +0100

    video-269

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -1,11 +1,24 @@
+import { useGetTodosQuery } from './store/apis';
+
 export const TodoApp = () => {
+	const { data: todos = [], isLoading } = useGetTodosQuery();
+
 	return (
 		<>
 			<h1>Todos - RTK Query</h1>
 			<hr />
-			<h4>IsLoading...</h4>
+			<h4>IsLoading {isLoading ? 'true' : 'false'}</h4>
 
 			<pre>...</pre>
+
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<strong>{todo.completed ? 'DONE ' : 'PENDING '}</strong>
+						{todo.title}
+					</li>
+				))}
+			</ul>
 
 			<button>Next TODO</button>
 		</>
