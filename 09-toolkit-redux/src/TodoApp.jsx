641b7a98b36f58e72f11d1a06b7d1fced5de6255
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-16 12:08:38 +0100

    Video-268

---
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -0,0 +1,13 @@
+export const TodoApp = () => {
+	return (
+		<>
+			<h1>Todos - RTK Query</h1>
+			<hr />
+			<h4>IsLoading...</h4>
+
+			<pre>...</pre>
+
+			<button>Next TODO</button>
+		</>
+	);
+};
