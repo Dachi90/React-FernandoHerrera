9306b4e5eeb96477dc685b3894e97fc69e1ab27e
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-22 17:29:01 +0100

    Video-285

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
 import { useForm } from '../../hooks';
-import { useState } from 'react';
+import { startCreatingUserWithEmailPassword } from '../../store/auth/thunks';
 
 const formata = {
 	email: '',
@@ -17,16 +19,17 @@ const formValidations = {
 };
 
 export const RegisterPage = () => {
+	const dispatch = useDispatch();
 	const [formSubmitted, setFormSubmitted] = useState(false);
 
 	const { displayName, email, password, onInputChange, formState, isFormValid, displayNameValid, emailValid, passwordValid } = useForm(formata, formValidations);
 
-	console.log(displayNameValid);
-
 	const onSubmit = (event) => {
 		event.preventDefault();
 		setFormSubmitted(true);
-		console.log(formState);
+
+		if (!isFormValid) return;
+		dispatch(startCreatingUserWithEmailPassword(formState));
 	};
 
 	return (
