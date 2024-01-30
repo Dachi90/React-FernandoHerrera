bba289cc10b45bca9fbc0ef44d167a998c56feaf
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-01-30 15:28:36 +0100

    Video-290

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { useSelector } from 'react-redux';
+import { CheckinhAuth } from '../ui/';
 
 export const AppRouter = () => {
+	const { status } = useSelector((state) => state.auth);
+
+	if (status === 'checking') {
+		return <CheckinhAuth />;
+	}
 	return (
 		<Routes>
 			{/* Login y regsitro */}
-			<Route path='/auth/*' element={<AuthRoutes />} />
+			<Route
+				path='/auth/*'
+				element={<AuthRoutes />}
+			/>
 
 			{/* JournalApp */}
-			<Route path='/*' element={<JournalRoutes />} />
+			<Route
+				path='/*'
+				element={<JournalRoutes />}
+			/>
 		</Routes>
 	);
 };
