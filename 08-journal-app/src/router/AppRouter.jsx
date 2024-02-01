22a8a93906ccee452e48aa6fd994d1b493817a1f
Martin Bergesio Trabajo <bergesio90@gmail.com>
2024-02-01 13:42:18 +0100

    Video-292

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { FirebaseAuth } from '../firebase/config';
-import { onAuthStateChanged } from 'firebase/auth';
-
-import { login, logout } from '../store/auth';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 import { CheckinhAuth } from '../ui/';
+import { UseCheckAuth } from '../hooks';
 
 export const AppRouter = () => {
-	const { status } = useSelector((state) => state.auth);
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		onAuthStateChanged(FirebaseAuth, async (user) => {
-			if (!user) return dispatch(logout());
-
-			const { uid, email, displayName, photoURL } = user;
-			dispatch(login({ uid, email, displayName, photoURL }));
-		});
-	}, []);
+	const { status } = UseCheckAuth();
 
 	if (status === 'checking') {
 		return <CheckinhAuth />;
