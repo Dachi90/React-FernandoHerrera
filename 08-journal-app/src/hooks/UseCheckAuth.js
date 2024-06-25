64826c4872807c32a4cf4c3ddf1cf782b360bdd6
Martín PC <bergesio90@gmail.com>
2024-06-25 21:49:34 +0200

    Video-303

--- 08-journal-app/src/hooks/UseCheckAuth.js
+++ 08-journal-app/src/hooks/UseCheckAuth.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAuthStateChanged } from 'firebase/auth';
 import { login, logout } from '../store/auth';
 import { FirebaseAuth } from '../firebase/config';
+import { startLoadingNotes } from '../store/journal';
 
 export const UseCheckAuth = () => {
 	const { status } = useSelector((state) => state.auth);
@@ -14,6 +15,7 @@ export const UseCheckAuth = () => {
 
 			const { uid, email, displayName, photoURL } = user;
 			dispatch(login({ uid, email, displayName, photoURL }));
+			dispatch(startLoadingNotes());
 		});
 	}, []);
 	return {
