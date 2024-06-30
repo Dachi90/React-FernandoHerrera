03706c4ba36132278c3add008600afdbac40a4bf
Martín PC <bergesio90@gmail.com>
2024-06-30 17:27:17 +0200

    Video-316

--- 08-journal-app/src/store/journal/journalSlice.js
+++ 08-journal-app/src/store/journal/journalSlice.js
@@ -50,9 +50,15 @@ export const journalSlice = createSlice({
 			state.active.imageUrls = [...state.active.imageUrls, ...action.payload];
 			state.isSaving = false;
 		},
+		clearNotesLogout: (state) => {
+			state.isSaving = false;
+			state.messageSaved = '';
+			state.notes = [];
+			state.active = null;
+		},
 		deleteNoteById: (state, action) => {},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById, savingNewNote, setPhotosToActiveNote } = journalSlice.actions;
+export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById, savingNewNote, setPhotosToActiveNote, clearNotesLogout } = journalSlice.actions;
