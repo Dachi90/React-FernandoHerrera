c23c2d40027864f65ce16e3915e8fda411009014
Martín PC <bergesio90@gmail.com>
2024-07-01 20:40:41 +0200

    Video-326

--- 08-journal-app/tests/helpers/fileUpload.test.js
+++ 08-journal-app/tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from '../../src/helpers/fileUpload';
+
+cloudinary.config({
+	cloud_name: 'dulumtlsf',
+	api_key: '566983683437449',
+	api_secret: 'pauOX20ENmAn9gT4Z_JK9iEGxFk',
+	secure: true,
+});
 
 describe('Pruebas en fileUpload', () => {
 	test('debe de subir el archivo correctamente a cloudinary', async () => {
@@ -9,5 +17,17 @@ describe('Pruebas en fileUpload', () => {
 
 		const url = await fileUpload(file);
 		expect(typeof url).toBe('string');
+
+		const segments = url.split('/');
+		const imageId = segments[segments.length - 1].replace('.png', '');
+		console.log(imageId);
+		await cloudinary.api.delete_resources([imageId]);
+	});
+
+	test('debe de retornar un null', async () => {
+		const file = new File([], 'foto.jpg');
+
+		const url = await fileUpload(file);
+		expect(url).toBe(null);
 	});
 });
