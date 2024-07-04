ca8855b23ada76588838392c4521bf569fd93bad
Martín PC <bergesio90@gmail.com>
2024-07-04 21:22:12 +0200

    Video-333

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -7,13 +7,25 @@ import { getFirestore } from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+//Dev/Prod
+// const firebaseConfig = {
+// 	apiKey: 'AIzaSyCYCE-0xsE43lsHMwfxyinCenqMya-wcK0',
+// 	authDomain: 'react-cursos-c0414.firebaseapp.com',
+// 	projectId: 'react-cursos-c0414',
+// 	storageBucket: 'react-cursos-c0414.appspot.com',
+// 	messagingSenderId: '950522999214',
+// 	appId: '1:950522999214:web:b067cbbe208bd9c624583f',
+// };
+
+//Testing
 const firebaseConfig = {
-	apiKey: 'AIzaSyCYCE-0xsE43lsHMwfxyinCenqMya-wcK0',
-	authDomain: 'react-cursos-c0414.firebaseapp.com',
-	projectId: 'react-cursos-c0414',
-	storageBucket: 'react-cursos-c0414.appspot.com',
-	messagingSenderId: '950522999214',
-	appId: '1:950522999214:web:b067cbbe208bd9c624583f',
+	apiKey: 'AIzaSyCgJKUhgWocH3B9w-YFQ2LAfRD5LJal0DM',
+	authDomain: 'react-cursos-test-49d30.firebaseapp.com',
+	projectId: 'react-cursos-test-49d30',
+	storageBucket: 'react-cursos-test-49d30.appspot.com',
+	messagingSenderId: '823907550934',
+	appId: '1:823907550934:web:45ab7b57e4ce7b6b5c2b77',
+	measurementId: 'G-T1MC13SBED',
 };
 
 // Initialize Firebase
