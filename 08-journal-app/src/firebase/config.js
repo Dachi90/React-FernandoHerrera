187d5c42c26fc748df9c9728f0eb2ed2ee43eac1
Martín PC <bergesio90@gmail.com>
2024-07-07 17:45:53 +0200

    Video-337

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -7,33 +7,19 @@ import { getEnvironments } from '../helpers/getEnvironments';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const env = getEnvironments();
-console.log(env);
+const { VITE_APIKEY, VITE_AUTHDOMAIN, VITE_PROJECTID, VITE_STORAGEBUCKET, VITE_MESSAGINGSENDERID, VITE_APPID, VITE_MEASUREMENTID } = getEnvironments();
 
-// console.log(process.env);
-// console.log(import.meta.env);
-
-// Your web app's Firebase configuration
-//Dev/Prod
-// const firebaseConfig = {
-// 	apiKey: 'AIzaSyCYCE-0xsE43lsHMwfxyinCenqMya-wcK0',
-// 	authDomain: 'react-cursos-c0414.firebaseapp.com',
-// 	projectId: 'react-cursos-c0414',
-// 	storageBucket: 'react-cursos-c0414.appspot.com',
-// 	messagingSenderId: '950522999214',
-// 	appId: '1:950522999214:web:b067cbbe208bd9c624583f',
-// };
-
-//Testing
 const firebaseConfig = {
-	apiKey: 'AIzaSyCgJKUhgWocH3B9w-YFQ2LAfRD5LJal0DM',
-	authDomain: 'react-cursos-test-49d30.firebaseapp.com',
-	projectId: 'react-cursos-test-49d30',
-	storageBucket: 'react-cursos-test-49d30.appspot.com',
-	messagingSenderId: '823907550934',
-	appId: '1:823907550934:web:45ab7b57e4ce7b6b5c2b77',
-	measurementId: 'G-T1MC13SBED',
+	apiKey: VITE_APIKEY,
+	authDomain: VITE_AUTHDOMAIN,
+	projectId: VITE_PROJECTID,
+	storageBucket: VITE_STORAGEBUCKET,
+	messagingSenderId: VITE_MESSAGINGSENDERID,
+	appId: VITE_APPID,
+	measurementId: VITE_MEASUREMENTID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
