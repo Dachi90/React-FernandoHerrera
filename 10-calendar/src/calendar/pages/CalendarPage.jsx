bf3b95941d70166fbcf126fbc3986a9894f65d3b
Martín PC <bergesio90@gmail.com>
2024-07-13 17:29:28 +0200

    Video-354

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -3,7 +3,7 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
-import { CalendarEvent, Navbar } from '../';
+import { CalendarEvent, CalendarModal, Navbar } from '../';
 
 import { localizer, getMessagesES } from '../../helpers';
 
@@ -69,6 +69,7 @@ export const CalendarPage = () => {
 				onSelectEvent={onSelect}
 				onView={onViewChanged}
 			/>
+			<CalendarModal />
 		</>
 	);
 };
