c8c1b51c21fba820eda73b2dc74e514aac0ec9a6
Martín PC <bergesio90@gmail.com>
2024-07-16 22:26:58 +0200

    Video-362

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { addHours, differenceInSeconds } from 'date-fns';
 
 import Swal from 'sweetalert2';
@@ -9,7 +9,7 @@ import Modal from 'react-modal';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { es } from 'date-fns/locale/es';
-import { useUiStore } from '../../hooks';
+import { useCalendarStore, useUiStore } from '../../hooks';
 
 registerLocale('es', es);
 
@@ -28,6 +28,7 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
 	const { isDateModalOpen, closeDateModal } = useUiStore();
+	const { activeEvent } = useCalendarStore();
 
 	const [formSubmitted, setFormSubmitted] = useState(false);
 
@@ -42,6 +43,12 @@ export const CalendarModal = () => {
 		if (!formSubmitted) return '';
 		return formValues.title.length > 0 ? '' : 'is-invalid';
 	}, [formValues.title, formSubmitted]);
+
+	useEffect(() => {
+		if (activeEvent !== null) {
+			setFormValues({ ...activeEvent });
+		}
+	}, [activeEvent]);
 
 	const onInputChange = ({ target }) => {
 		setFormValues({
--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onSetActiveEvent } from '../store';
 
 export const useCalendarStore = () => {
-	const dispatch = useDispatch;
+	const dispatch = useDispatch();
 	const { events, activeEvent } = useSelector((state) => state.calendar);
 
 	const setActiveEvent = (calendarEvent) => {
