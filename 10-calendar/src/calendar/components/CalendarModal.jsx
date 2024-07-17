7f315ee4805334f708a319ec66bd2cf41d237b5e
Martín PC <bergesio90@gmail.com>
2024-07-17 10:46:59 +0200

    Video-364

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -28,7 +28,7 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
 	const { isDateModalOpen, closeDateModal } = useUiStore();
-	const { activeEvent } = useCalendarStore();
+	const { activeEvent, startSavingEvent } = useCalendarStore();
 
 	const [formSubmitted, setFormSubmitted] = useState(false);
 
@@ -69,7 +69,7 @@ export const CalendarModal = () => {
 		closeDateModal();
 	};
 
-	const onSubmit = (event) => {
+	const onSubmit = async (event) => {
 		event.preventDefault();
 		setFormSubmitted(true);
 
@@ -84,8 +84,9 @@ export const CalendarModal = () => {
 		console.log(formValues);
 
 		//Todo:
-		// Remover errores en pantalla
-		// cerrar modal
+		await startSavingEvent(formValues);
+		closeDateModal();
+		setFormSubmitted(false);
 	};
 
 	return (
--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onSetActiveEvent } from '../store';
+import { onAddNewEvent, onSetActiveEvent } from '../store';
 
 export const useCalendarStore = () => {
 	const dispatch = useDispatch();
@@ -9,6 +9,18 @@ export const useCalendarStore = () => {
 		dispatch(onSetActiveEvent(calendarEvent));
 	};
 
+	const startSavingEvent = async (calendarEvent) => {
+		// TODO: llegar al backend
+
+		//Todo bien
+		if (calendarEvent._id) {
+			// Actualizando
+		} else {
+			// Creando
+			dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+		}
+	};
+
 	return {
 		//* Propiedades
 		events,
@@ -16,5 +28,6 @@ export const useCalendarStore = () => {
 
 		//* Métodos
 		setActiveEvent,
+		startSavingEvent,
 	};
 };
