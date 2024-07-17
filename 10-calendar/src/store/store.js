fbc15c25a8caa6ecdf4dd3046a9c4ca3bf750725
Martín PC <bergesio90@gmail.com>
2024-07-17 10:34:58 +0200

    Video-363

--- 10-calendar/src/store/store.js
+++ 10-calendar/src/store/store.js
@@ -2,10 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { calendarSlice, uiSlice } from './';
 
 export const store = configureStore({
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: false,
-		}),
+	// middleware: (getDefaultMiddleware) =>
+	// 	getDefaultMiddleware({
+	// 		serializableCheck: false,
+	// 	}),
 	reducer: {
 		calendar: calendarSlice.reducer,
 		ui: uiSlice.reducer,
