ee9ffa3025c483341f74c1fdaa4d80665ffcbcaf
Martín PC <bergesio90@gmail.com>
2024-07-19 12:06:47 +0200

    Video-379

--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -5,9 +5,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const router = Router();
-
+const { validarCAmpos } = require('../middlewares/validar-campos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
 
 router.post(
 	'/new',
@@ -16,6 +17,7 @@ router.post(
 		check('name', 'El nombre es obligatorio').not().isEmpty(),
 		check('email', 'El email es obligatorio').isEmail(),
 		check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+		validarCAmpos,
 	],
 	crearUsuario
 );
@@ -26,6 +28,7 @@ router.post(
 		// middlewares
 		check('email', 'El email es obligatorio').isEmail(),
 		check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+		validarCAmpos,
 	],
 	loginUsuario
 );
