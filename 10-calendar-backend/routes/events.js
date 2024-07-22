21a1fe24b6e1ff96762ebbce9026c27adddc4f20
Martín PC <bergesio90@gmail.com>
2024-07-22 13:50:42 +0200

    Video-395

--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -4,6 +4,10 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCAmpos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 
@@ -16,7 +20,16 @@ router.use(validarJWT);
 router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', crearEvento);
+router.post(
+	'/',
+	[
+		check('title', 'El título es obligatorio').not().isEmpty(),
+		check('start', 'fecha de inicio es obligatoria').custom(isDate),
+		check('end', 'fecha de finalización es obligatoria').custom(isDate),
+		validarCAmpos,
+	],
+	crearEvento
+);
 
 // Actualizar evento
 router.put('/:id', actualizarEvento);
