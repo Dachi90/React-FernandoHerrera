0a736edaa8736d7ee7df9567dd0f6c39f6f65e84
Martín PC <bergesio90@gmail.com>
2024-07-24 16:24:35 +0200

    Video-417

--- 10-calendar/src/auth/pages/LoginPage.jsx
+++ 10-calendar/src/auth/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const registerFormFields = {
 };
 
 export const LoginPage = () => {
-	const { startLogin, errorMessage } = useAuthStore();
+	const { startLogin, errorMessage, startRegister } = useAuthStore();
 	const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm(loginFormFields);
 	const {
 		registerName,
@@ -33,7 +33,11 @@ export const LoginPage = () => {
 
 	const registerSubmit = (event) => {
 		event.preventDefault();
-		console.log({ registerName, registerEmail, registerPassword, registerPassword2 });
+		if (registerPassword !== registerPassword2) {
+			Swal.fire('Error en registro', 'Las contraseñas no coinciden', 'error');
+			return;
+		}
+		startRegister({ name: registerName, email: registerEmail, password: registerPassword });
 	};
 
 	useEffect(() => {
