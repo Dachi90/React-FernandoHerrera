bca2083741729ecf39f6d1b2ebb2f53163e8d2c6
Martín PC <bergesio90@gmail.com>
2024-07-25 11:34:34 +0200

    Video-430

--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -34,10 +34,14 @@ export const useCalendarStore = () => {
 		}
 	};
 
-	const startDeletingEvent = () => {
-		//TODO: Llegar al backend
-
-		dispatch(onDeleteEvent());
+	const startDeletingEvent = async () => {
+		try {
+			await calendarApi.delete(`/events/${activeEvent.id}`);
+			dispatch(onDeleteEvent());
+		} catch (error) {
+			console.log(error);
+			Swal.fire('Error al eliminar el evento', error.response.data.msg, 'error');
+		}
 	};
 
 	const startLoadingEvents = async () => {
